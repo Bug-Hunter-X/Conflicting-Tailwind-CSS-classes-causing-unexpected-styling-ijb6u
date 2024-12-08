07df04458c771b516839d96a6f666212578cfeb6
BugHunter-X <askzenai@gmail.com>
2024-12-08 21:48:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,4 @@
+```javascript
+// Example of a Tailwind CSS class that causes an error due to an unexpected value
+<div class="bg-red-500 text-white p-4 rounded-lg w-64 bg-gradient-to-r from-blue-500 to-purple-500"></div>
+```
